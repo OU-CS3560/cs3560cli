1cfb9811c3563f35b7dfca982335dc1892fc6b2a
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2023-04-05 00:19:51 +0000

    feat: add create-gitignore, fix missing dep

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## v0.1.1
+
+- Add `create-gitignore` command.
+- Fix missing dependency for `watchdog`.
--- cs3560cli/main.py
+++ cs3560cli/main.py
@@ -2,6 +2,7 @@ import click
 
 from .functions.check_username import check_username
 from .functions.watch_zip import watch_zip
+from .functions.create_gitignore import create_gitignore
 
 @click.group()
 def cli():
@@ -9,6 +10,7 @@ def cli():
 
 cli.add_command(check_username)
 cli.add_command(watch_zip)
+cli.add_command(create_gitignore)
 
 if __name__ == "__main__":
     cli()
